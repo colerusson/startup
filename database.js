b464ddb2dba779f387afc1ef2912ce3c295312c4
colerusson <colerusson2@gmail.com>
2023-04-07 13:21:39 -0600

    edited database file

--- database.js
+++ database.js
@@ -13,8 +13,7 @@ if (!userName) {
 const url = `mongodb+srv://${userName}:${password}@${hostname}`;
 
 const client = new MongoClient(url);
-const userCollection = client.db('simon').collection('user');
-const scoreCollection = client.db('simon').collection('score');
+const userCollection = client.db('chatroom').collection('user');
 
 function getUser(email) {
   return userCollection.findOne({ email: email });
